213ecd5cba3b1a28d112d42ea8f3216e05aa651d
HuiBeom <gmlqja7913@daum.net>
2025-08-19 00:17:02 +0900

    tictactoe 순서, 승자 구현

--- src/Square.js
+++ src/Square.js
@@ -1,24 +1,14 @@
 import React from "react";
 import "./Square.css";
 
-// default로 해두면 메인으로 가져옴
-export default class Square extends React.Component{
-    // // 상태 저장은 state 이용
-    // // Board로 옮김
-    // constructor(props) {
-    //     super(props);
-    //     this.state = {
-    //         value: null,
-    //     }
-    // }
-
-    render() {
-        return(
-            <button className="square"
-                    onClick={() => {this.props.onClick()}}>
-                {/*props*/}
-                {this.props.value}
-            </button>
-        )
-    }
+// 함수형
+const Squares = ({onClick, value}) => {
+    return(
+        <button className="square"
+            onClick={onClick}>
+            {value}
+        </button>
+    )
 }
+export default Squares;
+
